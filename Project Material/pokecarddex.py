7f9a469ec4fc67960cc7e85be386ecaa5e4115c3
James Mertz <2474567+mertzjames@users.noreply.github.com>
2020-05-20 17:10:45 -0700

    updated pokedex.py with usage example.

--- Project Material/pokecarddex.py
+++ Project Material/pokecarddex.py
@@ -23,3 +23,18 @@ class PokeCardDex():
     def add_to_party(self, pokemon):
         pass
 
+
+
+# Below is an example usage for using the classes
+if __name__ == "__main__":
+    my_dex = PokeCardDex('pokemon_party.json')
+    rival_dex = PokeCardDex()
+    pikachu = Pokemon('Pikachu', 100, 'electric', None, None, (('electric charge', 30),))
+    rival_dex.add_to_party(pikachu)
+
+    my_dex.battle(rival_dex)
+
+    # for pokemon in my_dex.party:
+    #     print(pokemon.is_fainted)
+
+    my_dex.heal_party()
